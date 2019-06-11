7ad5dd3ac7d611d429fc6d8f44d19c2d43a60efa
Mateusz Derda <mateuszderda95@gmail.com>
2019-06-11 17:07:58 +0200

    add first migrate

--- driver_REST/driver_REST/settings.py
+++ driver_REST/driver_REST/settings.py
@@ -75,10 +75,16 @@ WSGI_APPLICATION = 'driver_REST.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'driver_db',
+        'USER': 'postgres',
+        'PASSWORD': 'coderslab',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+
     }
 }
+
 
 
 # Password validation
